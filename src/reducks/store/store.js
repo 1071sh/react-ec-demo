8320a3980d0ffa17129e23da771623f09b5592b1
nishida <i9i9ksy@gmail.com>
2021-04-06 12:03:51 +0900

    React-Redux入門#7

--- src/reducks/store/store.js
+++ src/reducks/store/store.js
@@ -1,13 +1,14 @@
-import { createStore as reduxCreateStore, combineReducers } from "redux";
-
+import { createStore as reduxCreateStore, combineReducers, applyMiddleware } from "redux";
+import { connectRouter, routerMiddleware } from "connected-react-router";
 import { UsersReducer } from "../users/reducers";
 
-const createStore = () => {
+const createStore = (history) => {
     return reduxCreateStore(
         combineReducers({
-            // products: ProductsReducer,
+            router: connectRouter(history),
             users: UsersReducer,
-        })
+        }),
+        applyMiddleware(routerMiddleware(history))
     );
 };
 
